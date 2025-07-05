eae6e56d2a346060eaaf2aae50b08a1ae4f6b64f
rishabh0009-7 <rizzlifts2024@gmail.com>
2025-07-05 19:06:48 +0530

    save coe snippet created

---
+++ app/actions/index.ts
@@ -0,0 +1,13 @@
+"use server";
+
+import { prisma } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+export const saveSnippet = async (id: number, code: string) => {
+  await prisma.snippet.update({
+    where: { id },
+    data: { code },
+  });
+
+  redirect(`/snippet/${id}`);
+};
--- components/EditSnippetForm.tsx
+++ components/EditSnippetForm.tsx
@@ -1,27 +1,35 @@
-"use client"
-import React, { useState } from 'react'
-import {Editor} from '@monaco-editor/react'
-import type { Snippet } from "@prisma/client"
-import { Button } from './ui/button'
+"use client";
 
-const EditSnippetForm = ({snippet}:{snippet :Snippet}) => {
-    const [Code , setCode ] = useState(snippet.code)
+import React, { useState } from "react";
+import { Editor } from "@monaco-editor/react";
+import type { Snippet } from "@/lib/generated/prisma";
+
+import { Button } from "./ui/button";
+import { saveSnippet } from "@/app/actions";
+
+const EditSnippetForm = ({ snippet }: { snippet: Snippet }) => {
+  const [code, setCode] = useState(snippet.code);
+
+ 
+
+  const saveSnippetAction = saveSnippet.bind(null, snippet.id, code);
+
   return (
-    <div className='flex flex-col gap-4'>
-        <form className='flex items-center justify-between'> 
-            <h1 className='font-bold text-xl '> Your code editor</h1>
-            <Button type='submit'>Save</Button>
-        </form>
+    <div className="flex flex-col gap-4">
+      <form action ={saveSnippetAction} className="flex items-center justify-between">
+        <h1 className="font-bold text-xl">Your Code Editor</h1>
+        <Button type="submit">Save</Button>
+      </form>
 
-        <Editor
-        
-        theme='vs-dark'
-        height = "40vh"
-        defaultLanguage = "javascript"
-        defaultValue={Code}
-        />
+      <Editor
+        theme="vs-dark"
+        height="40vh"
+        defaultLanguage="javascript"
+        value={code}
+        onChange={(value) => setCode(value || "")}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default EditSnippetForm
+export default EditSnippetForm;
